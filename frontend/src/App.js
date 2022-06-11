c04d9fbfd9f3af559663f2df5b69b3bcad7ccba2
Bhanu Arora <52175831+BhanuArora123@users.noreply.github.com>
2022-06-11 17:55:04 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import Editor from './pages/Editor';
 import { Toaster } from 'react-hot-toast';
-
 function App() {
   return (
     <>
